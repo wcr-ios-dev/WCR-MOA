b34d683895bf619418e4de8510b2197c501982c2
wcr-ios-dev <wcr-dev@wcrdevdeMacBook-Air.local>
2017-01-03 22:15:19 +0800

    解析方法

---
+++ WCR-MOA/Class/Login/PaserXml/LoginParser.h
@@ -0,0 +1,13 @@
+//
+//  LoginParser.h
+//  WCR-MOA
+//
+//  Created by wcr－dev on 2016/12/28.
+//  Copyright © 2016年 Alijoin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginParser : NSObject
+
+@end
---
+++ WCR-MOA/Class/Login/PaserXml/LoginParser.m
@@ -0,0 +1,13 @@
+//
+//  LoginParser.m
+//  WCR-MOA
+//
+//  Created by wcr－dev on 2016/12/28.
+//  Copyright © 2016年 Alijoin. All rights reserved.
+//
+
+#import "LoginParser.h"
+#import "TouchXML.h"
+@implementation LoginParser
+
+@end
